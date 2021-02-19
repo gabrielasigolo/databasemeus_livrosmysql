ef0f0de4a27f65fb92f57acc31fde382b7940244
Gabriela Sígolo <69328848+gabrielasigolo@users.noreply.github.com>
2021-02-19 11:50:07 -0300

    Update database meus_livros.sql

--- database meus_livros.sql
+++ database meus_livros.sql
@@ -1,29 +1,6 @@
 CREATE DATABASE  IF NOT EXISTS `meus_livros` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `meus_livros`;
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
---
--- Host: localhost    Database: meus_livros
--- ------------------------------------------------------
--- Server version	5.7.31
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `livros`
---
-
-DROP TABLE IF EXISTS `livros`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `livros` (
   `IDLivros` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(40) DEFAULT NULL,
@@ -36,25 +13,7 @@ CREATE TABLE `livros` (
   UNIQUE KEY `nome` (`nome`),
   UNIQUE KEY `nome_2` (`nome`)
 ) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `livros`
---
 
 LOCK TABLES `livros` WRITE;
-/*!40000 ALTER TABLE `livros` DISABLE KEYS */;
 INSERT INTO `livros` VALUES (17,'A máquina do tempo','H.G Wells',158,1895,'L&PM','Ficção Científica'),(18,'Respostas de um Astrofísico','Neil deGrasse Tyson',263,2020,'Record','Autoajuda'),(19,'Sob a Redoma','Stephen King',951,2009,'Suma de Letras','Ficção Científica'),(20,'Doctor Who: O prisioneiro dos Daleks','Trevor Baxendale',205,2009,'Suma de Letras','Ficção Científica'),(21,'Doctor Who: Cidade da Morte','James Goss',271,2015,'Suma de Letras','Ficção Científica'),(22,'Outsider','Stephen King',521,2018,'Charles Scribners Sons','Suspense'),(23,'Stranger Things: Raízes do Mal','Gwenda Bond',302,2019,'Intríseca','Terror'),(24,'O homem de giz','C.J. Tudor',269,2018,'Intríseca','Terror');
-/*!40000 ALTER TABLE `livros` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2021-02-02 14:07:57
